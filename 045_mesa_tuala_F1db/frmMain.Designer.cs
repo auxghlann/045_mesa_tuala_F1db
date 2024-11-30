55454458112f292c8c5c03f48eda420d8f4b05e4
auxghlann <orenoytemail@gmail.com>
2024-11-30 23:09:40 +0800

    feat: Add menuStrip and openfileDialog for generic database connection

--- 045_mesa_tuala_F1db/frmMain.Designer.cs
+++ 045_mesa_tuala_F1db/frmMain.Designer.cs
@@ -34,19 +34,23 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.importDbToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 35);
+            this.textBox1.Location = new System.Drawing.Point(67, 40);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(176, 22);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(265, 27);
+            this.button1.Location = new System.Drawing.Point(265, 32);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 38);
             this.button1.TabIndex = 1;
@@ -56,7 +60,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(67, 92);
+            this.dataGridView1.Location = new System.Drawing.Point(67, 97);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
@@ -65,7 +69,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(390, 387);
+            this.button2.Location = new System.Drawing.Point(390, 392);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(90, 53);
             this.button2.TabIndex = 3;
@@ -74,7 +78,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(642, 387);
+            this.button3.Location = new System.Drawing.Point(642, 392);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(90, 53);
             this.button3.TabIndex = 4;
@@ -83,12 +87,35 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(514, 387);
+            this.button4.Location = new System.Drawing.Point(514, 392);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(90, 53);
             this.button4.TabIndex = 4;
             this.button4.Text = "button3";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importDbToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(808, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // importDbToolStripMenuItem
+            // 
+            this.importDbToolStripMenuItem.Name = "importDbToolStripMenuItem";
+            this.importDbToolStripMenuItem.Size = new System.Drawing.Size(90, 24);
+            this.importDbToolStripMenuItem.Text = "import db";
+            this.importDbToolStripMenuItem.Click += new System.EventHandler(this.importDbToolStripMenuItem_Click);
             // 
             // frmMain
             // 
@@ -101,12 +128,16 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Record";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +151,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem importDbToolStripMenuItem;
     }
 }
 
--- 045_mesa_tuala_F1db/frmMain.cs
+++ 045_mesa_tuala_F1db/frmMain.cs
@@ -12,9 +12,27 @@ namespace _045_mesa_tuala_F1db
 {
     public partial class frmMain : Form
     {
+
+        private string db_file_path;
+        private Database db;
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void importDbToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "*.mdb|";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                db_file_path = dialog.FileName;
+
+                this.db = new Database(db_file_path);
+
+                MessageBox.Show("Successfully imported the mdb file", "Success", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
         }
     }
 }
