33d3357420618c7919e9505e3ce95d94acb48f76
naikom <naikom.2@DESKTOP-76S7633>
2024-11-30 22:16:38 +0800

    fix: ewan

--- 045_mesa_tuala_F1db/Database.cs
+++ 045_mesa_tuala_F1db/Database.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
 
 namespace _045_mesa_tuala_F1db
 {
     public class Database
     {
-        string ConnStr = "";
+
     }
 }
