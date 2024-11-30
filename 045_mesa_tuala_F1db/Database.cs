43bd0b45f131cd7646af3bbab7447788b2bc20f0
naikom <naikom.2@DESKTOP-76S7633>
2024-11-30 21:59:28 +0800

    Feat: Added a new form for inserting data into access db

---
+++ 045_mesa_tuala_F1db/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _045_mesa_tuala_F1db
+{
+    public class Database
+    {
+        string ConnStr = "";
+    }
+}
