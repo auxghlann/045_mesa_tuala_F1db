59e972882e5e72e6c2e2fd5d0c34ba350bfa87d7
auxghlann <orenoytemail@gmail.com>
2024-11-30 21:30:23 +0800

    Add project files.

---
+++ 045_mesa_tuala_F1db/frmMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _045_mesa_tuala_F1db
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
