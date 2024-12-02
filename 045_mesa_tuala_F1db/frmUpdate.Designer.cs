12c491200936497f6981dc1e8b284d81f28c73d9
auxghlann <orenoytemail@gmail.com>
2024-12-02 12:18:23 +0800

    refactor: Modify properties

--- 045_mesa_tuala_F1db/frmUpdate.Designer.cs
+++ 045_mesa_tuala_F1db/frmUpdate.Designer.cs
@@ -34,92 +34,130 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnCancelUpdate = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtModelUpdate = new System.Windows.Forms.TextBox();
+            this.txtPriceUpdate = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCurrModelDesc = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(54, 325);
+            this.btnUpdate.Location = new System.Drawing.Point(72, 400);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Size = new System.Drawing.Size(100, 28);
+            this.btnUpdate.TabIndex = 4;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // txtModeldescUpdate
             // 
-            this.txtModeldescUpdate.Location = new System.Drawing.Point(54, 76);
+            this.txtModeldescUpdate.Location = new System.Drawing.Point(72, 156);
+            this.txtModeldescUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtModeldescUpdate.Name = "txtModeldescUpdate";
-            this.txtModeldescUpdate.Size = new System.Drawing.Size(214, 20);
+            this.txtModeldescUpdate.Size = new System.Drawing.Size(284, 22);
             this.txtModeldescUpdate.TabIndex = 1;
             // 
             // cboBrandUpdate
             // 
+            this.cboBrandUpdate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboBrandUpdate.FormattingEnabled = true;
-            this.cboBrandUpdate.Location = new System.Drawing.Point(54, 177);
+            this.cboBrandUpdate.Location = new System.Drawing.Point(72, 280);
+            this.cboBrandUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboBrandUpdate.Name = "cboBrandUpdate";
-            this.cboBrandUpdate.Size = new System.Drawing.Size(214, 21);
-            this.cboBrandUpdate.TabIndex = 2;
+            this.cboBrandUpdate.Size = new System.Drawing.Size(284, 24);
+            this.cboBrandUpdate.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 161);
+            this.label1.Location = new System.Drawing.Point(68, 260);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(73, 16);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Brand";
+            this.label1.Text = "New Brand";
             // 
             // btnCancelUpdate
             // 
-            this.btnCancelUpdate.Location = new System.Drawing.Point(193, 325);
+            this.btnCancelUpdate.Location = new System.Drawing.Point(257, 400);
+            this.btnCancelUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancelUpdate.Name = "btnCancelUpdate";
-            this.btnCancelUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnCancelUpdate.TabIndex = 4;
+            this.btnCancelUpdate.Size = new System.Drawing.Size(100, 28);
+            this.btnCancelUpdate.TabIndex = 5;
             this.btnCancelUpdate.Text = "Cancel";
             this.btnCancelUpdate.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 60);
+            this.label2.Location = new System.Drawing.Point(68, 136);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.Size = new System.Drawing.Size(146, 16);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Model Description";
+            this.label2.Text = "New Model Description";
             // 
-            // txtModelUpdate
+            // txtPriceUpdate
             // 
-            this.txtModelUpdate.Location = new System.Drawing.Point(54, 127);
-            this.txtModelUpdate.Name = "txtModelUpdate";
-            this.txtModelUpdate.Size = new System.Drawing.Size(214, 20);
-            this.txtModelUpdate.TabIndex = 6;
+            this.txtPriceUpdate.Location = new System.Drawing.Point(72, 218);
+            this.txtPriceUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPriceUpdate.Name = "txtPriceUpdate";
+            this.txtPriceUpdate.Size = new System.Drawing.Size(284, 22);
+            this.txtPriceUpdate.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 111);
+            this.label3.Location = new System.Drawing.Point(68, 199);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.Size = new System.Drawing.Size(68, 16);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Price";
+            this.label3.Text = "New Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 76);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Model Desc to Update";
+            // 
+            // txtCurrModelDesc
+            // 
+            this.txtCurrModelDesc.Location = new System.Drawing.Point(71, 96);
+            this.txtCurrModelDesc.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCurrModelDesc.Name = "txtCurrModelDesc";
+            this.txtCurrModelDesc.Size = new System.Drawing.Size(284, 22);
+            this.txtCurrModelDesc.TabIndex = 8;
             // 
             // frmUpdate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 411);
+            this.ClientSize = new System.Drawing.Size(433, 506);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCurrModelDesc);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtModelUpdate);
+            this.Controls.Add(this.txtPriceUpdate);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnCancelUpdate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cboBrandUpdate);
             this.Controls.Add(this.txtModeldescUpdate);
             this.Controls.Add(this.btnUpdate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmUpdate";
-            this.Text = "frmDelete";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update Record";
+            this.Load += new System.EventHandler(this.frmUpdate_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,7 +171,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCancelUpdate;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtModelUpdate;
+        private System.Windows.Forms.TextBox txtPriceUpdate;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCurrModelDesc;
     }
 }
