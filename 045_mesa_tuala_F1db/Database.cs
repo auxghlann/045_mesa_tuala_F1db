766af5d939ccab8917a2bbaefaaa60ab71add265
auxghlann <orenoytemail@gmail.com>
2024-12-02 11:48:13 +0800

    feat: Add database query functions (get_table, add_record)

--- 045_mesa_tuala_F1db/Database.cs
+++ 045_mesa_tuala_F1db/Database.cs
@@ -25,7 +25,7 @@ namespace _045_mesa_tuala_F1db
 
         public Database(string file_path)
         {
-            string conn_str = provider + file_path; 
+            string conn_str = provider + file_path;
             connection = new OleDbConnection(conn_str);
         }
 
@@ -50,6 +50,40 @@ namespace _045_mesa_tuala_F1db
                 this.Connection.Close();
             }
 
+        }
+
+
+        // Query functions
+        public DataTable query_brand_table()
+        {
+            string query = "SELECT * FROM brand";
+
+            this.OpenConnection();
+
+            using (command = new OleDbCommand(query, this.Connection))
+            {
+                adapter = new OleDbDataAdapter(command);
+                dt = new DataTable();
+                adapter.Fill(dt);
+            }
+
+            this.CloseConnection();
+            return dt;
+        }
+
+        public int add_record(string model_desc, double price, int brand_id)
+        {
+            string query = "INSERT INTO model (model_desc, price, brandid) VALUES (@model_desc, @price, @brand_id)";
+            this.OpenConnection();
+            using (command = new OleDbCommand(query, this.Connection))
+            {
+                command.Parameters.AddWithValue("@model_desc", model_desc);
+                command.Parameters.AddWithValue("@price", price);
+                command.Parameters.AddWithValue("@brand_id", brand_id);
+                int result = command.ExecuteNonQuery();
+                this.CloseConnection();
+                return result;
+            }
         }
     }
 }
