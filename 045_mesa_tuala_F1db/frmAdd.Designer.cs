f391db0516d9ec6406d480853ef1cb6eeb99e0b5
auxghlann <orenoytemail@gmail.com>
2024-12-02 11:49:44 +0800

    refactor: Change properties

--- 045_mesa_tuala_F1db/frmAdd.Designer.cs
+++ 045_mesa_tuala_F1db/frmAdd.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
-            this.txtModelAdd = new System.Windows.Forms.TextBox();
+            this.txtPriceAdd = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,33 +41,37 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 105);
+            this.label3.Location = new System.Drawing.Point(55, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.Size = new System.Drawing.Size(38, 16);
             this.label3.TabIndex = 15;
             this.label3.Text = "Price";
             // 
-            // txtModelAdd
+            // txtPriceAdd
             // 
-            this.txtModelAdd.Location = new System.Drawing.Point(44, 121);
-            this.txtModelAdd.Name = "txtModelAdd";
-            this.txtModelAdd.Size = new System.Drawing.Size(214, 20);
-            this.txtModelAdd.TabIndex = 14;
+            this.txtPriceAdd.Location = new System.Drawing.Point(59, 149);
+            this.txtPriceAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPriceAdd.Name = "txtPriceAdd";
+            this.txtPriceAdd.Size = new System.Drawing.Size(284, 22);
+            this.txtPriceAdd.TabIndex = 14;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 54);
+            this.label2.Location = new System.Drawing.Point(55, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.Size = new System.Drawing.Size(116, 16);
             this.label2.TabIndex = 13;
             this.label2.Text = "Model Description";
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(183, 319);
+            this.btnCancel.Location = new System.Drawing.Point(244, 393);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(100, 28);
             this.btnCancel.TabIndex = 12;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -75,51 +79,62 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 155);
+            this.label1.Location = new System.Drawing.Point(55, 191);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(43, 16);
             this.label1.TabIndex = 11;
             this.label1.Text = "Brand";
             // 
             // cboBrandAdd
             // 
             this.cboBrandAdd.FormattingEnabled = true;
-            this.cboBrandAdd.Location = new System.Drawing.Point(44, 171);
+            this.cboBrandAdd.Location = new System.Drawing.Point(59, 210);
+            this.cboBrandAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboBrandAdd.Name = "cboBrandAdd";
-            this.cboBrandAdd.Size = new System.Drawing.Size(214, 21);
+            this.cboBrandAdd.Size = new System.Drawing.Size(284, 24);
             this.cboBrandAdd.TabIndex = 10;
             // 
             // txtModeldescAdd
             // 
-            this.txtModeldescAdd.Location = new System.Drawing.Point(44, 70);
+            this.txtModeldescAdd.Location = new System.Drawing.Point(59, 86);
+            this.txtModeldescAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtModeldescAdd.Name = "txtModeldescAdd";
-            this.txtModeldescAdd.Size = new System.Drawing.Size(214, 20);
+            this.txtModeldescAdd.Size = new System.Drawing.Size(284, 22);
             this.txtModeldescAdd.TabIndex = 9;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(44, 319);
+            this.btnAdd.Location = new System.Drawing.Point(59, 393);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
             this.btnAdd.TabIndex = 8;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // frmAdd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(303, 409);
+            this.ClientSize = new System.Drawing.Size(404, 503);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtModelAdd);
+            this.Controls.Add(this.txtPriceAdd);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cboBrandAdd);
             this.Controls.Add(this.txtModeldescAdd);
             this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmAdd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmAdd";
+            this.Load += new System.EventHandler(this.frmAdd_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,7 +143,7 @@
         #endregion
 
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtModelAdd;
+        private System.Windows.Forms.TextBox txtPriceAdd;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Label label1;
