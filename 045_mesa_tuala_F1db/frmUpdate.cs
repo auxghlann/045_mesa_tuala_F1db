707cffb0373c61e09cc188ff7eb3f5e79cd9463c
auxghlann <orenoytemail@gmail.com>
2024-12-02 12:18:13 +0800

    feat: Add functionalities to the update button

--- 045_mesa_tuala_F1db/frmUpdate.cs
+++ 045_mesa_tuala_F1db/frmUpdate.cs
@@ -12,9 +12,43 @@ namespace _045_mesa_tuala_F1db
 {
     public partial class frmUpdate : Form
     {
-        public frmUpdate()
+        Database db;
+        public frmUpdate(Database db)
         {
             InitializeComponent();
+            this.db = db;
+        }
+
+
+        // Helper Function
+
+        public void InitializeCBO()
+        {
+            DataTable dt = this.db.query_brand_table();
+            cboBrandUpdate.DataSource = dt;
+            cboBrandUpdate.DisplayMember = "brand";
+            cboBrandUpdate.ValueMember = "brandid";
+        }
+
+        // WinForm Function
+        private void frmUpdate_Load(object sender, EventArgs e)
+        {
+            InitializeCBO();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int rows_affectd = db.update_record(txtCurrModelDesc.Text, txtModeldescUpdate.Text, 
+                                    Convert.ToDouble(txtPriceUpdate.Text), Convert.ToInt32(cboBrandUpdate.SelectedValue.ToString()));
+
+            if (rows_affectd > 0)
+            {
+                MessageBox.Show("Successfully updated a record", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Failed to update a record", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
